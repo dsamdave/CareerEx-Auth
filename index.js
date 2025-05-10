ea9f49bc204584d3a9b5410b5a16e09ea58c4996
David Sampson <davidchosen33@gmail.com>
2025-05-10 19:17:23 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const bcrypt = require("bcryptjs")
+const Auth = require("./authModel")
+dotenv.config()
+
+
+
+const app = express()
+
+app.use(express.json())
+
+const PORT = process.env.PORT || 8000
+
+mongoose.connect(process.env.MONGODB_URL)
+.then(()=>{
+    console.log("Mongodb connected...")
+
+    app.listen(PORT, ()=>{
+        console.log(`Server started running on Port ${PORT}`)
+    }) 
+})
+
+
+app.post("/sign-up", async (req, res)=>{
+
+    try {
+        
+        const { email, password, firstName, lastName, state } = req.body
+
+        if(!email){
+            return res.status(400).json({message: "Please add your email"})
+        }
+    
+        if(!password){
+            return res.status(400).json({message: "Please enter password"})
+        }
+    
+        const existingUser = await Auth.findOne({ email })
+    
+        if(existingUser){
+            return res.status(400).json({message: "User account already exist"})
+        }
+    
+        if(password.length < 6){
+            return res.status(400).json({message: "Password should be a min of 6 chars"}) 
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12)
+    
+        const newUser = new Auth({ 
+            email, 
+            password: hashedPassword, 
+            firstName, 
+            lastName, 
+            state 
+        })
+    
+        await newUser.save()
+
+        // Send user Email
+    
+        res.status(201).json({
+            message: "User account created successfully",
+            newUser: { email, firstName, lastName, state }
+        })
+
+
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+
+})
